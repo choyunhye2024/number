c7e04cc9797905906c672c5c92b931d81e00f39b
choyunhye2024 <jdbsgp1221@gmail.com>
2024-08-22 15:21:40 +0900

    Add files via upload

---
+++ PersonalNumber/src/com/personalnumber/LengthExample.java
@@ -0,0 +1,18 @@
+package com.personalnumber;
+
+public class LengthExample {
+
+	public static void main(String[] args) {
+
+		String ssn = "9512212000000";
+		int length = ssn.length();
+		if (length == 13) {
+
+			System.out.println("주민등록번호 자릿수가 맞습니다.");
+		} else {
+
+			System.out.println("주민등록번호 자릿수가 틀립니다.");
+		}
+	}
+
+}
